e60a7155ac2683e4e96d3e45330865eb2e54e5a8
Salvatore Rendo <salvo.strit@gmail.com>
2023-11-18 22:14:13 +0100

    Update solve.py

--- olicyber-training/web/11-Csrf-token/solve.py
+++ olicyber-training/web/11-Csrf-token/solve.py
@@ -1,6 +1,6 @@
 import requests
-import json
 
+#Log in to get coockies
 url_credentials = 'http://web-11.challs.olicyber.it/login'
 
 s = requests.Session()
@@ -9,6 +9,7 @@ body = {'username':'admin', 'password':'admin'}
 
 r = s.post(url=url_credentials,json=body)
 
+#Extract session cookie and csrf token
 session_coockie = r.cookies
 
 csrf_value = r.json()['csrf']
@@ -17,6 +18,8 @@ url_flag='http://web-11.challs.olicyber.it/flag_piece'
 
 flag = ''
 
+#extract the flag pieces using session cookie and
+# csrf token that change at every get to the resource
 for i in range(4):
 
     r = s.get(url=(url_flag+'?csrf='+csrf_value+'&index='+str(i)),
